b9bc8c75df36247da183b8d18d17916842c9b678
Alexander Gutkin <agutkin@google.com>
2026-03-07 17:37:17 +0000

    File path with normalization.

--- openfst/compat/file_path.cc
+++ openfst/compat/file_path.cc
@@ -21,6 +21,7 @@
 
 #include "absl/strings/match.h"
 #include "absl/strings/str_cat.h"
+#include "absl/strings/str_replace.h"
 #include "absl/strings/string_view.h"
 #include "absl/strings/strip.h"
 
@@ -30,53 +31,68 @@ namespace {
 constexpr std::string_view kPathSeparator = "/";
 
 std::pair<std::string_view, std::string_view> SplitPath(std::string_view path) {
-  size_t pos = path.find_last_of(kPathSeparator[0]);
+  const size_t pos = path.find_last_of(kPathSeparator[0]);
 
   // Handle the case with no '/' in 'path'.
-  if (pos == std::string_view::npos)
+  if (pos == std::string_view::npos) {
     return std::make_pair(path.substr(0, 0), path);
-
+  }
   // Handle the case with a single leading '/' in 'path'.
-  if (pos == 0)
+  if (pos == 0) {
     return std::make_pair(path.substr(0, 1), absl::ClippedSubstr(path, 1));
-
+  }
   return std::make_pair(path.substr(0, pos),
                         absl::ClippedSubstr(path, pos + 1));
+}
+
+std::string Normalize(std::string_view path) {
+#if defined(_WIN32)
+  // Don't mix separators on Windows for consistency.
+  return absl::StrReplaceAll(path, {{"\\", kPathSeparator}});
+#else
+  return std::string(path);
+#endif  // _WIN32
 }
 
 }  // namespace
 
 std::string JoinPath(std::string_view path1, std::string_view path2) {
-  if (path1.empty()) return std::string(path2);
-  if (path2.empty()) return std::string(path1);
+  const std::string norm_path2 = Normalize(path2);
+  if (path1.empty()) return norm_path2;
+  const std::string norm_path1 = Normalize(path1);
+  if (path2.empty()) return norm_path1;
 
-  if (absl::EndsWith(path1, kPathSeparator)) {
-    return absl::StrCat(path1, absl::StripPrefix(path2, kPathSeparator));
+  if (absl::EndsWith(norm_path1, kPathSeparator)) {
+    return absl::StrCat(norm_path1,
+                        absl::StripPrefix(norm_path2, kPathSeparator));
   } else {
-    if (absl::StartsWith(path2, kPathSeparator)) {
-      return absl::StrCat(path1, path2);
+    if (absl::StartsWith(norm_path2, kPathSeparator)) {
+      return absl::StrCat(norm_path1, norm_path2);
     } else {
-      return absl::StrCat(path1, kPathSeparator, path2);
+      return absl::StrCat(norm_path1, kPathSeparator, norm_path2);
     }
   }
 }
 
 std::string JoinPath(std::string_view path1, std::string_view path2,
                      std::string_view path3) {
-  return JoinPath(JoinPath(path1, path2), path3);
+  return JoinPath(JoinPath(Normalize(path1), Normalize(path2)),
+                  Normalize(path3));
 }
 
 std::string JoinPathRespectAbsolute(std::string_view path1,
                                     std::string_view path2) {
-  if (path1.empty()) return std::string(path2);
-  if (absl::StartsWith(path2, kPathSeparator)) {
-    return std::string(path2);
+  const std::string norm_path2 = Normalize(path2);
+  if (path1.empty()) return norm_path2;
+  if (absl::StartsWith(norm_path2, kPathSeparator)) {
+    return norm_path2;
   }
-  return JoinPath(path1, path2);
+  const std::string norm_path1 = Normalize(path1);
+  return JoinPath(norm_path1, norm_path2);
 }
 
-std::string_view Basename(std::string_view path) {
-  return SplitPath(path).second;
+std::string Basename(std::string_view path) {
+  return std::string(SplitPath(Normalize(path)).second);
 }
 
 }  // namespace fst
